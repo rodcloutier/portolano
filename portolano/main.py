72e246d6e013bad037340fd69c3f56ebd1787c05
Rod Cloutier <rodcloutier@gmail.com>
2017-05-13 17:07:21 -0400

    Initial serving of static index.yaml

---
+++ portolano/main.py
@@ -0,0 +1,9 @@
+import connexion
+from connexion.resolver import RestyResolver
+import os.path
+
+specification_dir = os.path.join(os.path.dirname(__file__), 'api')
+
+app = connexion.App(__name__, specification_dir='api/')
+app.add_api('api.yaml', resolver=RestyResolver('api'))
+app.run(port=8080)
